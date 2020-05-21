61ada4942a55bbc4221d2760a62cb7a8a1515b4d
Douglas Naphas <douglasnaphas@gmail.com>
2020-05-21 17:09:07 -0400

    Add ret-from-async.use.js

--- experiments/ret-from-async/ret-from-async.use.js
+++ experiments/ret-from-async/ret-from-async.use.js
@@ -2,7 +2,23 @@ const retFromAsync = require("./ret-from-async");
 
 // console.log(retFromAsync()); // prints Promise { <pending> }
 
+const retFromAsync2 = async () => {
+  return Promise.resolve({
+    userId: 1,
+    id: 1,
+    title: "sun aut facere",
+    body: "quia et suscipit"
+  });
+};
+
 (async () => {
   const data = await retFromAsync();
   console.log(data); // prints the data as desired
 })();
+
+(async () => {
+  const data2 = await retFromAsync2();
+  console.log(data2);
+})();
+
+// console.log(retFromAsync2()); // prints Promise { <pending> }
