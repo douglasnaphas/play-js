aa35d1023ac26f4e40611cf21f27efbb20bfc42c
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-26 09:40:07 -0500

    Show hoisting of vars, not consts

---
+++ hoisting/hoist.js
@@ -0,0 +1,7 @@
+console.log(x); // undefined
+x = "pre";
+console.log(x); // pre
+var x = "echs";
+console.log(x); // echs
+// console.log(y); // cannot access 'y' before initialization
+ const y = "why";
