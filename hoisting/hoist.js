1e093e45af1be9ab096709de590d32e2f70553d2
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-26 09:43:11 -0500

    Show function scoping of var

--- hoisting/hoist.js
+++ hoisting/hoist.js
@@ -4,4 +4,10 @@ console.log(x); // pre
 var x = "echs";
 console.log(x); // echs
 // console.log(y); // cannot access 'y' before initialization
- const y = "why";
+const y = "why";
+// console.log(z); // z is not defined
+function useZ() {
+  var z = "zet";
+}
+useZ();
+// console.log(z); // z is not defined
